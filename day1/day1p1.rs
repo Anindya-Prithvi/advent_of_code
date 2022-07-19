7088d29f9ef062cd03eaca9cd84b1001f4545d18
Andy Candy <anindya20024@iiitd.ac.in>
2022-07-19 20:44:39 +0530

    aoc with rust ig lol

---
+++ day1/day1p1.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main(){
+    let input = fs::read_to_string("input1.txt").expect("Unable to read file");
+    let splits = input.trim().lines().flat_map(str::parse::<i32>);
+    let inputvec = splits.collect::<Vec<i32>>();
+    let len = inputvec.len();
+    let mut totcount:i32 = 0;
+    for i in 1..len {
+        if inputvec[i] > inputvec[i-1] {
+            totcount += 1;
+        }
+    }
+    println!("{}",totcount);
+}
